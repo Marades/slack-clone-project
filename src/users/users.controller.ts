688c98a44e68846a774bdbbb5d46ad0b7ce32fee
김건회 <jkl9510357@gmail.com>
2021-07-20 00:00:38 +0900

    커스텀 데코레이터

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { User } from 'src/common/decorator/user.decorator';
 import { UserDto } from 'src/common/dto/user.dto';
 import { JoinRequestDto } from './dto/join.request.dto';
 import { UsersService } from './users.service';
@@ -17,9 +18,8 @@ export class UsersController {
     })
     @ApiOperation({ summary: '내 정보 조회' })
     @Get()
-    getUsers(@Req() req) {
-
-        return req.user
+    getUsers(@User() user) {
+        return user
     }
 
     @ApiOperation({ summary: '회원가입' })
